2eabf9edc72658c3574f24717a4e04ea6f02128f
RobertSilver <robert.silver@outlook.com>
2014-02-01 20:58:29 +0000

    more updates.

--- LeatherheadCompWeb/App_Start/BundleConfig.cs
+++ LeatherheadCompWeb/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace LeatherheadCompWeb
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/CSS/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/CSS/*.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
