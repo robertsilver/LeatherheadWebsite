69e8ff6e6398790da00d724a52e5b866c1bb88e7
RobertSilver <robert.silver@outlook.com>
2014-02-02 11:04:26 +0000

    more changes

--- LeatherheadCompWeb/App_Start/BundleConfig.cs
+++ LeatherheadCompWeb/App_Start/BundleConfig.cs
@@ -1,7 +1,6 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
-namespace LeatherheadCompWeb
+namespace LeatherheadCompWeb.App_Start
 {
     public class BundleConfig
     {
@@ -9,19 +8,22 @@ namespace LeatherheadCompWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/Lib/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //            "~/Scripts/Lib/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/Lib/jquery.unobtrusive*",
+            //            "~/Scripts/Lib/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Lib").Include(
+                     "~/Scripts/App/lhc.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/Lib/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/CSS/*.css"));
 
