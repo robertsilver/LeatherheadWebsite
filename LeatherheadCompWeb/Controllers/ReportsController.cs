185e9a5fa513e170b722ba38c6d4215338095890
RobertSilver <robert.silver@profilegroup.co.uk>
2014-03-04 15:00:59 +0000

    altered code to add the Reports.

---
+++ LeatherheadCompWeb/Controllers/ReportsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeatherheadCompWeb.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
